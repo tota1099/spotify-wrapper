0a83e163032c75ebe9067282f6f667d6aae342c1
Renan Porto <renan.porto1099@gmail.com>
2019-04-09 19:58:09 -0300

    Added scripts to generate the dist by UMD

---
+++ webpack.config.babel.js
@@ -0,0 +1,19 @@
+import { join } from 'path';
+
+const include = join(__dirname, 'src');
+
+export default {
+  mode: 'development',
+  entry: './src/index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', include },
+    ],
+  },
+};
